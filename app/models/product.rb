c581bc2c1699f436863875a1858017c7e3ba1adc
ziraqyoung <ziraqyoung@outlook.com>
2020-04-13 13:34:26 +0300

    Add validations and tests to product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,9 @@
 class Product < ApplicationRecord
+  validates :title, uniqueness: true
+  validates :title, :description, :image_url, presence: true
+  validates :price, numericality: { greater_than_or_equal_to: 0.01 }
+  validates :image_url, allow_blank: true, format: {
+                with: %r{\.(gif|jpg|png)\Z}i,
+                message: "must be a valid URL for PNG, JPG or GIF image",
+              }
 end
