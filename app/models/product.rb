d812d16a37a9815e308c34744db0c50b5cb60d73
ziraqyoung <ziraqyoung@outlook.com>
2020-04-15 21:29:46 +0300

    Add cart, line_items and store scaffolds

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ApplicationRecord
+  has_many :line_items
+
   validates :title, uniqueness: true
   validates :title, :description, :image_url, presence: true
   validates :price, numericality: { greater_than_or_equal_to: 0.01 }
@@ -6,4 +8,14 @@ class Product < ApplicationRecord
                 with: %r{\.(gif|jpg|png)\Z}i,
                 message: "must be a valid URL for PNG, JPG or GIF image",
               }
+  before_destroy :ensure_not_referenced_by_any_line_item
+
+  private
+    # ensure that there are no line_items referencing this product
+    def ensure_not_referenced_by_any_line_item
+      unless line_items.empty?
+        errors.add(:base, 'Line Items Present')
+        throw :abort
+      end
+    end
 end
