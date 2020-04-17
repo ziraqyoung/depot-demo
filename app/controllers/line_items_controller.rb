51ff67aef0969e8dce1a18295e1a9cdecd2f9860
ziraqyoung <ziraqyoung@outlook.com>
2020-04-17 17:37:21 +0300

    - Make cart functional ajax-base

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -32,7 +32,8 @@ class LineItemsController < ApplicationController
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to @line_item.cart }
+        format.js { @current_item = @line_item }
+        format.html { redirect_to store_index_url }
         format.json { render :show, status: :created, location: @line_item }
       else
         format.html { render :new }
